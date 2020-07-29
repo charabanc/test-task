02971f948c47f928be5184075b372c3ec34b4c69
charabanc <59258850+charabanc@users.noreply.github.com>
2020-07-29 21:28:35 +0300

    Собрал билд

---
+++ build/precache-manifest.ddcac7043db397f1ccbcbb13a5f3105d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f2b9160ef63982f227878fa934cef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c69a316107fa01d55be3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ee29f2c49872d70753d9",
+    "url": "/static/js/2.e5315bf7.chunk.js"
+  },
+  {
+    "revision": "bb0d2e924b7b6743d70dbdc0ece34a4f",
+    "url": "/static/js/2.e5315bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69a316107fa01d55be3",
+    "url": "/static/js/main.cc384a07.chunk.js"
+  },
+  {
+    "revision": "4d9af2a9620265095b18",
+    "url": "/static/js/runtime-main.e8bda96d.js"
+  }
+]);
